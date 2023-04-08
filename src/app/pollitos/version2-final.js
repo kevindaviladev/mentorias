5d68a8d7e7bcbca637d9a35ec4fe3668cca44cd8
Kevin Davila <kfariidavila@gmail.com>
2023-04-08 09:21:05 -0500

    Added Pollitos logic

---
+++ src/app/pollitos/version2-final.js
@@ -0,0 +1,49 @@
+Por dia tendremos n lotes
+
+lote = {
+    fecha: new Date(),
+    machosContados: 1000,
+    hembraContadas: 1000
+}
+
+
+dia0607 = [lote1, lote2]
+dia0707 = [lote1, lote2]
+
+//N dias
+produccion = [dias0607, dia0707...]
+
+// 10/04
+
+production = [dia1004];
+
+// 12/04
+dias = [{}, dia1104, dia1204];
+
+//15/04
+production = [
+    dia1004, dia1104, dia1204, dia1304,  dia1404,  dia1504
+];
+
+// 20/06
+// RESTAR FECHA ACTUAL - LA FECHA INICIAL = CANTIDAD DE DIAS
+// CANTIDAD DE DIAS + 1 = POSICION EN EL ARRAY => 40
+
+production[40];
+dia2006 = [lote1, lote2]
+
+for i of dia2006 {
+
+    dia2006[i] = {
+        fecha: new Date(),
+        machosContados: 1000,
+        hembraContadas: 1000
+    }
+    if dia2006[i] !== 0 {
+        // Log
+        LOG.cantidadTotal = dia2006[i].machosContados + dia2006[i].hembrasContadas
+        LOG.machosContados = dia2006[i].machosContados;
+        LOG.hembraContadas = dia2006[i].hembraContadas;
+    }
+}
+
